09d87dc90acc19fcd08d996b1f4e3ecdc383e517
GoodJinMo <kaa415@naver.com>
2025-02-05 22:44:18 +0900

    2025

---
+++ src/components/PostCreate/CreatePost2.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-container fluid >
+    <v-sheet color="light-green-lighten-5" class="pa-5">
+      <v-text-field placeholder="Title" variant="underlined"></v-text-field>
+    <v-textarea
+      v-model="Post_Content"
+      bg-color="light-green-lighten-5"
+      variant="solo"
+      rows="30"
+      auto-grow
+    ></v-textarea>
+    </v-sheet>
+  
+  </v-container>
+</template>
+  
+  <script setup>
+  //type="submit"
+  import { ref,onMounted} from 'vue'
+  import { useCounterStore } from '@/store/DataManager.js'
+  const store =useCounterStore()
+ 
+  const Post_Content=ref("")
+  </script>
+  <style scoped>
+
+  </style>
--- src/views/CreatePostPage.vue
+++ src/views/CreatePostPage.vue
@@ -4,6 +4,7 @@
   
   <script setup>
     import CreatePost from '@/components/PostCreate/CreatePost.vue'
+    import CreatePost2 from '@/components/PostCreate/CreatePost2.vue'
     import CreatePostAppbar from '@/components/PostCreate/CreatePostAppbar.vue'
   </script>
   
